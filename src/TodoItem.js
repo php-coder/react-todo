eaf39f9a7818ce0dc07f6550283296dbcfd2c8a1
Slava Semushin <slava.semushin@gmail.com>
2018-05-28 19:22:24 +0200

    Implement simple mechanism for marking task as done.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,13 +5,30 @@ class TodoItem extends Component {
   constructor(props) {
     super(props);
     this.name = props.name;
+    this.state = {
+      active: true
+    }
   }
 
   render() {
     return (
-      <li>{ this.name }</li>
+      <li>
+        <label>
+          <input type="checkbox" onClick={ this.markItemAsDone } disabled={ !this.state.active } />
+          {
+            this.state.active
+            ? this.name
+            : <del>{ this.name }</del>
+          }
+        </label>
+      </li>
     )
   }
+
+  markItemAsDone = () => {
+    this.setState({ active: false });
+  }
+
 }
 
 export default TodoItem;
